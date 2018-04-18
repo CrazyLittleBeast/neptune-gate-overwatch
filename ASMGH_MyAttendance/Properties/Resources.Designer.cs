204246dbf410f8e5152289da3c8810489a7037cd
crazylittlebeast <cerberus.outlaw@gmail.com>
2018-04-18 08:23:53 +0800

    Stabiity update with provincial logo

--- ASMGH_MyAttendance/Properties/Resources.Designer.cs
+++ ASMGH_MyAttendance/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace ASMGH_MyAttendance.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap province_log117 {
+            get {
+                object obj = ResourceManager.GetObject("province_log117", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
